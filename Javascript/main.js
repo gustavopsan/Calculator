db304b437d66eae8c3569943df1e3291304c81b6
gustavopsan <gustavopsantana4@gmail.com>
2020-03-03 21:41:19 -0300

    Changes #3 (changing jquery)

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,16 +1,12 @@
 $(document).ready(function () {
+
     console.log("ready");
-    $(".j_number").click(function () {
-        console.log(this.value);
-    })
-    $(".j_operator").click(function () {
-        console.log(this.value);
-    })
-    $(".j_clear").click(function () {
-        console.log(this.value);
-        $(".j_tela").val('');
-    })
-    $(".j_equal").click(function () {
-        console.log(this.value);
-    })
+
+    $('.j_number').click(function () { 
+        $('.tela2').val($('.tela2').val() + (this).value);
+    });
+
+    $('.j_clear').click(function () {
+        $('.j_tela').val('');
+    });
 })
