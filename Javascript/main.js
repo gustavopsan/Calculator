8734d07b67fa2093c215ad7161838b3ba1f9de9b
gustavopsan <gustavopsantana4@gmail.com>
2020-03-11 16:57:57 -0300

    Instanciando elementos em variáveis no javascript

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
 
     let Verifier = 0;
-
     let num1, num2, result = 0;
+    var history = $('#tela1');
+    var visor = $('#tela2');
 
     console.log("ready");
 
     $('.j_number').click(function () { 
-        $('.tela2').val($('.tela2').val() + (this).value);
+        visor.val(visor.val() + (this).value);
     });
 
     $('#j_clear').click(function () {
-        $('.tela1').val('');
-        $('.tela2').val(null);
-        console.log($('.tela2').val());
+        history.val('');
+        visor.val('');
         num1 = 0;
         num2 = 0;
         result = 0;
@@ -22,9 +22,9 @@ $(document).ready(function () {
     $('#j_sum').click(function () {
 
         Verifier = 1;
-        num1 = $('.tela2').val();
-        $('.tela1').val(num1 + '+');
-        $('.tela2').val('');
+        num1 = visor.val();
+        history.val(num1 + '+');
+        visor.val('');
     })
 
     $('#j_sub').click(function () {
@@ -32,9 +32,9 @@ $(document).ready(function () {
             $('#tela2').val('-');
         } else {
             Verifier = 2;
-            num1 = $('.tela2').val();
-            $('.tela1').val(num1 + '-');
-            $('.tela2').val(''); 
+            num1 = visor.val();
+            history.val(num1 + '-');
+            visor.val(''); 
         }
          
     })
@@ -42,17 +42,17 @@ $(document).ready(function () {
     $('#j_prod').click(function () {
 
         Verifier = 3;
-        num1 = $('.tela2').val();
-        $('.tela1').val(num1 + '×');
-        $('.tela2').val('');
+        num1 = visor.val();
+        history.val(num1 + '×');
+        visor.val('');
     })
 
     $('#j_div').click(function () {
 
         Verifier = 4;
-        num1 = $('.tela2').val();
-        $('.tela1').val(num1 + '÷');
-        $('.tela2').val('');
+        num1 = visor.val();
+        history.val(num1 + '÷');
+        visor.val('');
     })
 
     $('#j_equal').click( function () {
@@ -60,10 +60,10 @@ $(document).ready(function () {
         switch (Verifier) {
             case 1:
                 console.log('sum = true');
-                num2 = $('.tela2').val();
+                num2 = visor.val();
                 result = parseFloat(num1) + parseFloat(num2);
-                $('.tela1').val(num1 + '+' + num2);
-                $('.tela2').val(parseFloat(result.toFixed(2)));
+                history.val(num1 + '+' + num2);
+                visor.val(parseFloat(result.toFixed(2)));
                 num1 = 0;
                 num2 = 0;
                 result = 0;
@@ -72,10 +72,10 @@ $(document).ready(function () {
         
             case 2:
                 console.log('subtract = true');
-                num2 = $('.tela2').val();
+                num2 = visor.val();
                 result = num1 - num2;
-                $('.tela1').val(num1 + '-' + num2);
-                $('.tela2').val(parseFloat(result.toFixed(2)));
+                history.val(num1 + '-' + num2);
+                visor.val(parseFloat(result.toFixed(2)));
                 num1 = 0;
                 num2 = 0;
                 result = 0;
@@ -83,10 +83,10 @@ $(document).ready(function () {
                 break;
             case 3:
                 console.log('multiplication = true')
-                num2 = $('.tela2').val();
+                num2 = visor.val();
                 result = parseFloat(num1) * parseFloat(num2);
-                $('.tela1').val(num1 + '×' + num2);
-                $('.tela2').val(parseFloat(result.toFixed(2)));
+                history.val(num1 + '×' + num2);
+                visor.val(parseFloat(result.toFixed(2)));
                 num1 = 0;
                 num2 = 0;
                 result = 0;
@@ -94,10 +94,10 @@ $(document).ready(function () {
                 break;
             case 4:
                 console.log('division = true')
-                num2 = $('.tela2').val();
+                num2 = visor.val();
                 result = parseFloat(num1) / parseFloat(num2);
-                $('.tela1').val(num1 + '÷' + num2);
-                $('.tela2').val(parseFloat(result.toFixed(2)));
+                history.val(num1 + '÷' + num2);
+                visor.val(parseFloat(result.toFixed(2)));
                 num1 = 0;
                 num2 = 0;
                 result = 0;
